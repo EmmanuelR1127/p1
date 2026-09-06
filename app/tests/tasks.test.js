977cafa83237ad4fb442ef6864dac84598b6c6bd
emmanuel <emmanuel.rougier11@gmail.com>
2026-09-06 15:35:50 +0200

    ajout tests base API

---
+++ app/tests/tasks.test.js
@@ -0,0 +1,12 @@
+const request = require("supertest");
+const app = require("../server");
+
+describe("GET/tasks", () => {
+  it("devrait retourner une liste de tâches", async () => {
+    const response = await
+request(app).get("/tasks");
+
+    expect(response.statusCode).toBe(200);
+    expect(Array.isArray(response.body)).toBe(true);
+});
+});
